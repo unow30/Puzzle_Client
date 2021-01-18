b03fe796c9a2354998d06d9b1dc198c90e23a3d7
devminsukim <cvnefr7704@gmail.com>
2021-01-18 14:31:12 +0900

    update style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import {ThemeProvider} from 'styled-components'
+import GlobalStyles from './style/GlobalStyles'
+import theme from './style/theme'
+import Home from './components/page/Home/Home'
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${props => props.color || 'black'};
-  border-radius: 50%;
-  ${props =>
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
-`
-
-function App() {
-  return <Circle color="red" huge />
+const App = () => {
+  return(
+    <ThemeProvider theme={theme}>
+      <Home></Home>
+    <GlobalStyles/>
+    </ThemeProvider>
+  )
 }
 
 export default App
