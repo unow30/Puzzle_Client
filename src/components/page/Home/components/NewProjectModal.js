78c8f6e366c7f4828d4bfff77383aa31366c0ea3
jo-turn <94jhgmsg@gmail.com>
2021-01-28 02:17:05 +0900

    update homepage

--- src/components/page/Home/components/NewProjectModal.js
+++ src/components/page/Home/components/NewProjectModal.js
@@ -7,6 +7,13 @@ import axios from 'axios'
 
 export const NewProjectModal = ({ showNewProject, setShowNewProject }) => {
   const modalRef = useRef()
+  const [addMember, setAddMember] = useState(false)
+  const [currMem, setCurrMem] = useState('김코딩')
+  // const [currMem, setCurrMem] = useState(['김코딩'])
+
+  const addMemberHandle = () => {
+    setAddMember(prev => !prev)
+  }
 
   const animation = useSpring({
     config: {
@@ -74,10 +81,35 @@ export const NewProjectModal = ({ showNewProject, setShowNewProject }) => {
                   placeholder="프로젝트 내용"
                 />
               </ProjectInfoBox>
+
               <ProjectMemberInfo>프로젝트 인원</ProjectMemberInfo>
-              <Memberbtn>멤버 추가</Memberbtn>
+              <AddMemberbtn onClick={addMemberHandle}>멤버 추가</AddMemberbtn>
+              <div>
+                {addMember ? (
+                  <>
+                    <AddMemberInfo
+                      onChange={onChange}
+                      name="usercode"
+                      type="text"
+                      placeholder="Usercode를 입력해주세요. "
+                    />
+                    <AddUserCodebtn>추가</AddUserCodebtn>
+                  </>
+                ) : null}
+              </div>
+
+              <CurrMemBox>
+                <CurrMem>{currMem}</CurrMem>
+              </CurrMemBox>
+
+              <CloseModalButton
+                aria-label="Close modal"
+                onClick={() => setShowNewProject(prev => !prev)}
+              />
+              <CreateProjectbtnBox></CreateProjectbtnBox>
             </ModalWrapper>
           </animated.div>
+          <CreateProjectbtn>프로젝트 생성</CreateProjectbtn>
         </Background>
       ) : null}
     </>
@@ -94,7 +126,7 @@ const Background = styled.div`
 
 const ModalWrapper = styled.div`
   width: 800px;
-  height: 500px;
+  height: 700px;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
   background: #052439;
   position: relative;
@@ -115,6 +147,18 @@ const ModalWrapper = styled.div`
   }
 `
 
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+  color: white;
+`
+
 const Title = styled.div`
   font-size: 2rem;
   color: white;
@@ -127,7 +171,7 @@ const ProjectListImg = styled.div`
   background-image: url(${Img});
   width: 150px;
   height: 150px;
-  border-radius: 50%;
+  border-radius: 5%;
   background-size: cover;
   background-color: white;
   margin: 0 auto;
@@ -164,6 +208,24 @@ const ProjectInfo = styled.input`
   }
   font-size: 1.1rem;
 `
+const AddMemberInfo = styled.input`
+  background-color: transparent;
+  border: transparent;
+  border-bottom: 3px solid #afafaf;
+  padding-bottom: 10px;
+  color: white;
+  width: 10vw;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: #afafaf;
+  }
+  font-size: 0.9rem;
+  position: absolute;
+  right: 10vw;
+  top: 38vh;
+`
 const ProjectInfoBox = styled.div`
   position: absolute;
   display: flex;
@@ -176,7 +238,7 @@ const ProjectInfoBox = styled.div`
   height: 8vh;
   background-color: transparent;
 `
-const Memberbtn = styled.button`
+const AddMemberbtn = styled.button`
   width: 60px;
   height: 30px;
   color: white;
@@ -188,6 +250,7 @@ const Memberbtn = styled.button`
   position: absolute;
   right: 10vw;
   top: 34.5vh;
+
   &:active,
   &:focus {
     outline: none;
@@ -199,4 +262,63 @@ const ProjectMemberInfo = styled.div`
   position: absolute;
   top: 35vh;
   right: 16.7vw;
+`
+
+const CreateProjectbtn = styled.button`
+  width: 260px;
+  height: 25px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1em;
+  color: #111;
+  outline: none;
+  background-color: #fa991d;
+  position: absolute;
+  right: 10vw;
+  bottom: 10px;
+  display: block;
+  &:hover {
+    color: white;
+  }
+`
+const CreateProjectbtnBox = styled.div`
+  height: 500px;
+`
+
+const CurrMem = styled.div`
+  font-size: 1em;
+  color: white;
+  text-align: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin: 0 auto;
+`
+
+const CurrMemBox = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
+  right: 18.48vw;
+  top: 41vh;
+  height: 8vh;
+  border-top: 3px solid #fa991d;
+`
+
+const ProjectMemberInfoBox = styled.div``
+
+const AddUserCodebtn = styled.span`
+  color: white;
+  margin-left: 2px;
+  cursor: pointer;
+  position: absolute;
+  right: 10vw;
+  top: 38vh;
+  font-size: 0.9rem;
 `
